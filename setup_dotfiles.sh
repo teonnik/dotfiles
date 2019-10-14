968f201089fc26f56c27cf5a0c0ccac370baa9fd
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-10-14 22:24:27 +0200

    Fix setup script
    - create directories if they don't exist
    - relink if file exists

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -3,19 +3,28 @@
 # get the dir of the current script
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -s $script_dir/gdb.conf ~/.gdbinit
-ln -s $script_dir/tmux.conf ~/.tmux.conf
-ln -s $script_dir/vim/vim.conf  ~/.vimrc
-ln -s $script_dir/vim/bulgarian-phonetic.vim ~/.vim/keymap/bulgarian-phonetic.vim
-ln -s $script_dir/zsh.conf ~/.zshrc
-ln -s $script_dir/git.conf ~/.gitconfig
-ln -s $script_dir/latexmkrc.conf ~/.latexmkrc
-ln -s $script_dir/i3.conf ~/.config/i3/config
-ln -s $script_dir/kde/wm.conf ~/.config/plasma-workspace/env/wm.sh 
-ln -s $script_dir/kde_shortcuts.conf ~/.config/kglobalshortcutsrc  
-ln -s $script_dir/kwin.conf ~/.config/kwinrc
-ln -s $script_dir/polybar/config.ini ~/.config/polybar/config
-ln -s $script_dir/polybar/launch.sh ~/.config/polybar/launch.sh 
-ln -s $script_dir/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-ln -s $script_dir/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-ln -s $script_dir/compton/compton.conf ~/.config/compton.conf: 
+mkdir -p ~/.vim/keymap
+mkdir -p ~/.config
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/plasma-workspace
+mkdir -p ~/.config/polybar
+mkdir -p ~/.config/sxhkd
+mkdir -p ~/.config/bspwm
+
+
+ln -sf $script_dir/gdb.conf ~/.gdbinit
+ln -sf $script_dir/tmux.conf ~/.tmux.conf
+ln -sf $script_dir/vim/vim.conf  ~/.vimrc
+ln -sf $script_dir/vim/bulgarian-phonetic.vim ~/.vim/keymap/bulgarian-phonetic.vim
+ln -sf $script_dir/zsh.conf ~/.zshrc
+ln -sf $script_dir/git.conf ~/.gitconfig
+ln -sf $script_dir/latexmkrc.conf ~/.latexmkrc
+ln -sf $script_dir/i3.conf ~/.config/i3/config
+ln -sf $script_dir/kde/wm.conf ~/.config/plasma-workspace/env/wm.sh 
+#ln -sf $script_dir/kde_shortcuts.conf ~/.config/kglobalshortcutsrc  
+#ln -sf $script_dir/kwin.conf ~/.config/kwinrc
+ln -sf $script_dir/polybar/config.ini ~/.config/polybar/config
+ln -sf $script_dir/polybar/launch.sh ~/.config/polybar/launch.sh 
+ln -sf $script_dir/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln -sf $script_dir/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+ln -sf $script_dir/compton/compton.conf ~/.config/compton.conf
