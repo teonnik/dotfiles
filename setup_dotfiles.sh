fe72a4fc4f4f2efc462b60c13c47613fb820f6fc
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-11-30 11:55:31 +0100

    Remove kde/plasma, bspwm, sxhkd, compton

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,12 +4,8 @@
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 mkdir -p ~/.vim/keymap
-mkdir -p ~/.config
 mkdir -p ~/.config/i3
-mkdir -p ~/.config/plasma-workspace
 mkdir -p ~/.config/polybar
-mkdir -p ~/.config/sxhkd
-mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/waybar
 mkdir -p ~/.config/waybar/modules
 mkdir -p ~/.config/sway
@@ -22,15 +18,10 @@ ln -sf $script_dir/vim/bulgarian-phonetic.vim ~/.vim/keymap/bulgarian-phonetic.v
 ln -sf $script_dir/zsh.conf ~/.zshrc
 ln -sf $script_dir/git.conf ~/.gitconfig
 ln -sf $script_dir/latexmkrc.conf ~/.latexmkrc
+
 ln -sf $script_dir/i3.conf ~/.config/i3/config
-ln -sf $script_dir/kde/wm.conf ~/.config/plasma-workspace/env/wm.sh 
-#ln -sf $script_dir/kde_shortcuts.conf ~/.config/kglobalshortcutsrc  
-#ln -sf $script_dir/kwin.conf ~/.config/kwinrc
 ln -sf $script_dir/polybar/config.ini ~/.config/polybar/config
 ln -sf $script_dir/polybar/launch.sh ~/.config/polybar/launch.sh 
-ln -sf $script_dir/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-ln -sf $script_dir/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-ln -sf $script_dir/compton/compton.conf ~/.config/compton.conf
 
 ln -sf $script_dir/sway/config              ~/.config/sway/config
 ln -sf $script_dir/waybar/config.json       ~/.config/waybar/config
