4594a886d326b7b2a7e8482a0b8276808e032fd1
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-11-30 11:59:55 +0100

    Remove tmux
    - Change formatting for setup dotfiles

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -11,20 +11,19 @@ mkdir -p ~/.config/waybar/modules
 mkdir -p ~/.config/sway
 
 
-ln -sf $script_dir/gdb.conf ~/.gdbinit
-ln -sf $script_dir/tmux.conf ~/.tmux.conf
-ln -sf $script_dir/vim/vim.conf  ~/.vimrc
-ln -sf $script_dir/vim/bulgarian-phonetic.vim ~/.vim/keymap/bulgarian-phonetic.vim
-ln -sf $script_dir/zsh.conf ~/.zshrc
-ln -sf $script_dir/git.conf ~/.gitconfig
-ln -sf $script_dir/latexmkrc.conf ~/.latexmkrc
+ln -sf $script_dir/gdb.conf                     ~/.gdbinit
+ln -sf $script_dir/zsh.conf                     ~/.zshrc
+ln -sf $script_dir/git.conf                     ~/.gitconfig
+ln -sf $script_dir/latexmkrc.conf               ~/.latexmkrc
+ln -sf $script_dir/vim/vim.conf                 ~/.vimrc
+ln -sf $script_dir/vim/bulgarian-phonetic.vim   ~/.vim/keymap/bulgarian-phonetic.vim
 
-ln -sf $script_dir/i3.conf ~/.config/i3/config
-ln -sf $script_dir/polybar/config.ini ~/.config/polybar/config
-ln -sf $script_dir/polybar/launch.sh ~/.config/polybar/launch.sh 
+ln -sf $script_dir/i3.conf                      ~/.config/i3/config
+ln -sf $script_dir/polybar/config.ini           ~/.config/polybar/config
+ln -sf $script_dir/polybar/launch.sh            ~/.config/polybar/launch.sh 
 
-ln -sf $script_dir/sway/config              ~/.config/sway/config
-ln -sf $script_dir/waybar/config.json       ~/.config/waybar/config
-ln -sf $script_dir/waybar/style.css         ~/.config/waybar/style.css
-ln -sf $script_dir/waybar/modules/kblayout  ~/.config/waybar/modules/kblayout
+ln -sf $script_dir/sway/config                  ~/.config/sway/config
+ln -sf $script_dir/waybar/config.json           ~/.config/waybar/config
+ln -sf $script_dir/waybar/style.css             ~/.config/waybar/style.css
+ln -sf $script_dir/waybar/modules/kblayout      ~/.config/waybar/modules/kblayout
 
