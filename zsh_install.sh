e99aa5d31fc94b483cf76a1f5f7ef1af5f1f2833
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-12-04 19:14:35 +0100

    Install zsh plugins

---
+++ zsh_install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Oh-My-Zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Spaceship theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
