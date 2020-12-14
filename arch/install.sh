aa8d3e3955baa84414f1bf049798dc3ea8553e4e
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-12-14 11:12:14 +0100

    Improve the Arch install script

--- arch/install.sh
+++ arch/install.sh
@@ -6,43 +6,38 @@
 # Update the system clock
 timedatectl set-ntp true
 
-# TODO: partition the disks
-# TODO: format the partitions
+# Use `fdisk /dev/sdx` to partition the disk
+#
+# /dev/sdx1 : partition for booting
+# - size: 512M
+# - type: EFI System
+#
+# /dev/sdx2 : Arch OS partition
+# - size: remaining
+# - type: Linux Filesystem
+
+# Format the partitions
+#
+# mkfs.fat -F32 /dev/sdx1
+# mkfs.ext4 /dev/sdx2
+
+# Mount partitions:
+#
+# mount /dev/sdx2 /mnt
+# mkdir /mnt/boot
+# mount /dev/sdx1 /mnt/boot
+
 # TODO: edit mirrorlist
 
 # Install base packages
-pacstrap /mnt base linux linux-firmware
-
-# Generate an fstab file
-genfstab -U /mnt >> /mnt/etc/fstab
-
-
-# ------------------------------
-# Installation
-
-# change root
-arch-chroot /mnt
-
-# Time zone
-ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
-hwclock --systohc
-
-# Localization TODO
-
-# Root password
-passwd
-
-# Add a user
-useradd -m teonnik
-
-# Set pacman's Color option
-sed -i '/Color/s/^#//g' /etc/pacman.conf
-
-# Create local folders
-mkdir -p ~/code ~/build ~/downloads ~/bin ~/install ~/software
-
-# Install official Arch packages
 packages=(
+  linux           # linux kernel and modules
+  linux-firmware  # firmware files
+
+  base         # bundle: bash, file, grep, gzip, iproute2, pacman, sed, shadow, systemd, tar, util-linux
+  util-linux   # bundle: chsh, cal, column, fdisk, hwclock, kill, lsblk, lscpu, mkfs, mount, su, umount
+  base-devel   # bundle: autoconf, automake, binutils, make, which, gcc, sudo, pkgconf
+
   grub        # boot loader
   efibootmgr  # EFI boot manager
 
@@ -56,18 +51,14 @@ packages=(
   rofi                  # app launcher
   grim                  # screenshot utility
   mako libnotify        # notifications
+  waybar                # bar
   #wev                   # debugging Wayland events
 
-  # lscpu
-  util-linux
-
-  xdg-user-dirs
-
+  alacritty             # terminal emulator
+  xdg-user-dirs         # user directory manager
   at                    # schedule commands
+  hexyl                 # command line hex viewer
 
-  hexyl          # command line hex viewer
-
-  base-devel     # basic tools: make, which, gcc, grep, sudo, sed, etc
   zsh            # shell
   gvim           # editor
   ninja          # alternative to make
@@ -75,7 +66,8 @@ packages=(
   git            # version control management
   cmake          # build tool
   diff-so-fancy  # better diff
-  clang openmp   # clang with opnemp support
+  clang          # compiler
+  openmp         # clang's opnemp support
 
   # monitoring
   perf
@@ -86,10 +78,10 @@ packages=(
   nethogs         # net top tool
   mtr             # traceroute alternative
 
-  gnupg  # encryption and signing tool
-  pass   # password store
-  lastpass-cli
-  pwgen  # generate passwords from the command line
+  gnupg        # encryption and signing tool
+  pass         # password store
+  lastpass-cli # LastPass CLI password store
+  pwgen        # generate passwords from the command line
 
   # vpn
   networkmanager-openconnect
@@ -166,7 +158,6 @@ packages=(
 
 
   firefox               # browser
-  qutebrowser
   nm-connection-editor  # network manager GUI
   keepassxc             # password manager
   konsole               # terminal emulator
@@ -180,11 +171,59 @@ packages=(
   inkscape              # vector graphics editor
   gimp                  # raster graphics editor
 
-  # no longer used: texstudio gwenview okular dolphin feh
+  # no longer used: texstudio gwenview okular dolphin feh qutebrowser
 )
-pacman --noconfirm --needed -S  ${packages[@]}
+#pacman --noconfirm --needed -S  ${packages[@]}
+pacstrap /mnt ${packages[@]}
 
-# Install yay
+# Generate the fstab file
+genfstab -U /mnt >> /mnt/etc/fstab
+
+
+
+# ------------------------------
+# Post install
+
+# Change root
+arch-chroot /mnt
+
+# Time zone
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+hwclock --systohc
+
+# Localization
+sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# Network
+echo "teonnik" >> /etc/hostname
+echo "127.0.1.1 teonnik.localdomain teonnik" >> /etc/hosts
+
+# Bootloader
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Set pacman's Color option
+sed -i '/Color/s/^#//g' /etc/pacman.conf
+
+# Services
+systemctl enable sddm.service \
+                 NetworkManager.service \
+                 bluetooth.service \
+                 syncthing@teonnik.service \
+                 atd.service
+#                org.cups.cupsd.service
+
+# Root password
+passwd
+
+# Add a user
+useradd -m -a -G wheel,video -s /usr/bin/zsh teonnik
+passwd teonnik
+# TODO: edit /etc/sudoers to allow for the wheel group
+
+# AUR package manager
 git clone https://aur.archlinux.org/yay.git
 (cd yay; makepkg -si)
 
@@ -196,11 +235,19 @@ aur_pkgs=(
   slack-desktop
   navi-bin
   gcalcli
-  mutt-wizard-git
   direnv
   ripgrep-all
 )
 yay --noconfirm --needed -S ${aur_pkgs[@]}
+
+
+
+
+# ------------------------------
+# Local install
+
+# Create local folders
+mkdir -p ~/code ~/build ~/downloads ~/bin ~/install ~/software
 
 # spack
 git clone https://github.com/spack/spack.git ~/code
@@ -209,47 +256,34 @@ git clone https://github.com/spack/spack.git ~/code
 git clone https://github.com/teonnik/dotfiles.git ~/code
 ~/code/dotfiles/setup.sh
 
-# TODO: `firefox` addons: NoScript (?), LastPass, Bypass Paywalls, Adblock Plus
+# `firefox` addons: LastPass, Bypass Paywalls, AdBlocker Ultimate
 
-
-# ------------------------------
-# vim
+# ssh keys
+ssh-keygen -t ed25519 -C "teodor.nikolov22@gmail.com"
 
 # install vim plugin manager
 curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # install vim plugins
 vim +PlugInstall +qall
+
 # install YouCompleteMe
 (cd ${HOME}/.vim/bundle/YouCompleteMe; python3 install.py --clangd-completer)
+
 # install bulgarian phonetic
 curl -fLo ${HOME}/.vim/keymap/bulgarian-phonetic.vim --create-dirs http://www.math.bas.bg/bantchev/vim/bulgarian-phonetic.vim
 
-# ------------------------------
-# zsh
+# make zsh default shell : FIXME: doesn't work ??
+#chsh -s $(which zsh)
 
-# make zsh default shell
-chsh -s $(which zsh)
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# install powerlevel10k
+
+# install powerlevel10k: `p10k configure`
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # install zsh autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Services
-systemctl enable sddm.service \
-                 bluetooth.service \
-                 org.cups.cupsd.service \
-                 NetworkManager.service \
-                 syncthing@teonnik.service \
-                 atd.service
-
-# Fix for `light` on Arch
-usermod -aG video <your-user>
-
 # TODO: xdg-user-dirs ?
-
-# Reboot
-exit
-reboot
