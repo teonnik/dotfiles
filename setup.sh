b46d6f2a5162f089732c5fc0edf42b3c27f7d804
Teodor Nikolov <teodor.nikolov22@gmail.com>
2021-12-28 17:27:46 +0200

    Add the -n option to avoid to avoid duplicate symlinks
    - This avoids creating a symlink inside a directory, or symlink to a
      directory that already exists with the target name

--- setup.sh
+++ setup.sh
@@ -6,40 +6,40 @@ CONF=${HOME}/.config
 mkdir -p ${HOME}/.newsboat
 mkdir -p ${HOME}/.xkb/symbols
 
-ln -sf ${DOTS}/alacritty                    ${CONF}/alacritty
-ln -sf ${DOTS}/gdb/gdb.conf                 ${HOME}/.gdbinit
-ln -sf ${DOTS}/git/gitconfig                ${HOME}/.gitconfig
-ln -sf ${DOTS}/git/gitignore_global         ${HOME}/.gitignore_global
-ln -sf ${DOTS}/kanshi                       ${CONF}/kanshi
-ln -sf ${DOTS}/latex/latexmkrc.conf         ${HOME}/.latexmkrc
-ln -sf ${DOTS}/mbsyncrc                     ${HOME}/.mbsyncrc
-ln -sf ${DOTS}/msmtprc                      ${HOME}/.msmtprc
-ln -sf ${DOTS}/neomutt                      ${CONF}/neomutt
-ln -sf ${DOTS}/newsboat/urls                ${HOME}/.newsboat/urls
-ln -sf ${DOTS}/newsboat/config              ${HOME}/.newsboat/config
-ln -sf ${DOTS}/nvim                         ${CONF}/nvim
-ln -sf ${DOTS}/sway                         ${CONF}/sway
-ln -sf ${DOTS}/swaylock                     ${CONF}/swaylock
+ln -sfn ${DOTS}/alacritty                    ${CONF}/alacritty
+ln -sfn ${DOTS}/gdb/gdb.conf                 ${HOME}/.gdbinit
+ln -sfn ${DOTS}/git/gitconfig                ${HOME}/.gitconfig
+ln -sfn ${DOTS}/git/gitignore_global         ${HOME}/.gitignore_global
+ln -sfn ${DOTS}/kanshi                       ${CONF}/kanshi
+ln -sfn ${DOTS}/latex/latexmkrc.conf         ${HOME}/.latexmkrc
+ln -sfn ${DOTS}/mbsyncrc                     ${HOME}/.mbsyncrc
+ln -sfn ${DOTS}/msmtprc                      ${HOME}/.msmtprc
+ln -sfn ${DOTS}/neomutt                      ${CONF}/neomutt
+ln -sfn ${DOTS}/newsboat/urls                ${HOME}/.newsboat/urls
+ln -sfn ${DOTS}/newsboat/config              ${HOME}/.newsboat/config
+ln -sfn ${DOTS}/nvim                         ${CONF}/nvim
+ln -sfn ${DOTS}/sway                         ${CONF}/sway
+ln -sfn ${DOTS}/swaylock                     ${CONF}/swaylock
 # TODO: syncthing
-ln -sf ${DOTS}/vim/vim.conf                 ${HOME}/.vimrc
-ln -sf ${DOTS}/waybar                       ${CONF}/waybar
-ln -sf ${DOTS}/wofi                         ${CONF}/wofi
-ln -sf ${DOTS}/xkb/us-german-umlaut         ${HOME}/.xkb/symbols/us-german-umlaut
-ln -sf ${DOTS}/zathura                      ${CONF}/zathura
-ln -sf ${DOTS}/zsh/zshrc                    ${HOME}/.zshrc
+ln -sfn ${DOTS}/vim/vim.conf                 ${HOME}/.vimrc
+ln -sfn ${DOTS}/waybar                       ${CONF}/waybar
+ln -sfn ${DOTS}/wofi                         ${CONF}/wofi
+ln -sfn ${DOTS}/xkb/us-german-umlaut         ${HOME}/.xkb/symbols/us-german-umlaut
+ln -sfn ${DOTS}/zathura                      ${CONF}/zathura
+ln -sfn ${DOTS}/zsh/zshrc                    ${HOME}/.zshrc
 
-ln -sf ${DOTS}/mimeapps.list                ${CONF}/mimeapps.list
-ln -sf ${DOTS}/xdg_applications             ${HOME}/.local/share/applications
-ln -sf ${DOTS}/wget                         ${CONF}/wget
+ln -sfn ${DOTS}/mimeapps.list                ${CONF}/mimeapps.list
+ln -sfn ${DOTS}/xdg_applications             ${HOME}/.local/share/applications
+ln -sfn ${DOTS}/wget                         ${CONF}/wget
 
-ln -sf ${DOTS}/profile                      ${HOME}/.zprofile
-ln -sf ${DOTS}/profile                      ${HOME}/.bash_profile
+ln -sfn ${DOTS}/profile                      ${HOME}/.zprofile
+ln -sfn ${DOTS}/profile                      ${HOME}/.bash_profile
 
 # -- bin
-ln -sf ${DOTS}/bin/colorpicker              ${HOME}/bin/colorpicker
-ln -sf ${DOTS}/bin/scr                      ${HOME}/bin/scr
-ln -sf ${DOTS}/bin/scrmonitor               ${HOME}/bin/scrmonitor
-ln -sf ${DOTS}/bin/scrwindow                ${HOME}/bin/scrwindow
-ln -sf ${DOTS}/bin/slack                    ${HOME}/bin/slack
-ln -sf ${DOTS}/bin/pack                     ${HOME}/bin/pack
-ln -sf ${DOTS}/bin/unpack                   ${HOME}/bin/unpack
+ln -sfn ${DOTS}/bin/colorpicker              ${HOME}/bin/colorpicker
+ln -sfn ${DOTS}/bin/scr                      ${HOME}/bin/scr
+ln -sfn ${DOTS}/bin/scrmonitor               ${HOME}/bin/scrmonitor
+ln -sfn ${DOTS}/bin/scrwindow                ${HOME}/bin/scrwindow
+ln -sfn ${DOTS}/bin/slack                    ${HOME}/bin/slack
+ln -sfn ${DOTS}/bin/pack                     ${HOME}/bin/pack
+ln -sfn ${DOTS}/bin/unpack                   ${HOME}/bin/unpack
