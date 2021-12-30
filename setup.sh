d1a19c4988da7a54483b56f9f9e793913b1cf8b5
Teodor Nikolov <teodor.nikolov22@gmail.com>
2021-12-30 11:05:06 +0200

    Make the repo structure follow $HOME structure

--- setup.sh
+++ setup.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
 
 DOTS=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-CONF=${HOME}/.config
 
+lndot() {
+  ln -sfn ${DOTS}/${1} ${HOME}/${2:-${1}}
+}
+
+mkdir -p ${HOME}/bin
 mkdir -p ${HOME}/.xkb/symbols
+mkdir -p ${HOME}/.config
+#mkdir -p ${HOME}/.local/share/applications
 
-ln -sfn ${DOTS}/alacritty                    ${CONF}/alacritty
-ln -sfn ${DOTS}/gdb                          ${CONF}/gdb
-ln -sfn ${DOTS}/git                          ${CONF}/git
-ln -sfn ${DOTS}/kanshi                       ${CONF}/kanshi
-ln -sfn ${DOTS}/isync                        ${CONF}/isync
-ln -sfn ${DOTS}/msmtp                        ${CONF}/msmtp
-ln -sfn ${DOTS}/neomutt                      ${CONF}/neomutt
-ln -sfn ${DOTS}/newsboat                     ${CONF}/newsboat
-ln -sfn ${DOTS}/notmuch                      ${CONF}/notmuch
-ln -sfn ${DOTS}/nvim                         ${CONF}/nvim
-ln -sfn ${DOTS}/sway                         ${CONF}/sway
-ln -sfn ${DOTS}/swaylock                     ${CONF}/swaylock
-ln -sfn ${DOTS}/waybar                       ${CONF}/waybar
-ln -sfn ${DOTS}/wofi                         ${CONF}/wofi
-ln -sfn ${DOTS}/zathura                      ${CONF}/zathura
-ln -sfn ${DOTS}/mimeapps.list                ${CONF}/mimeapps.list
-ln -sfn ${DOTS}/wget                         ${CONF}/wget
-ln -sfn ${DOTS}/foot                         ${CONF}/foot
-ln -sfn ${DOTS}/latexmk                      ${CONF}/latexmk
+lndot .config/alacritty
+lndot .config/foot
+lndot .config/gdb
+lndot .config/git
+lndot .config/isync
+lndot .config/kanshi
+lndot .config/latexmk
+lndot .config/msmtp
+lndot .config/mimeapps.list
+lndot .config/neomutt
+lndot .config/newsboat
+lndot .config/notmuch
+lndot .config/nvim
+lndot .config/sway
+lndot .config/swaylock
+lndot .config/waybar
+lndot .config/wget
+lndot .config/wofi
+lndot .config/zathura
 
-ln -sfn ${DOTS}/vim/vimrc                    ${HOME}/.vim/vimrc
-ln -sfn ${DOTS}/xkb/us-german-umlaut         ${HOME}/.xkb/symbols/us-german-umlaut
-ln -sfn ${DOTS}/zsh/zshrc                    ${HOME}/.zshrc
-ln -sfn ${DOTS}/xdg_applications             ${HOME}/.local/share/applications
-ln -sfn ${DOTS}/profile                      ${HOME}/.zprofile
-ln -sfn ${DOTS}/profile                      ${HOME}/.bash_profile
+lndot .profile      .zprofile
+lndot .profile      .bash_profile
+lndot .vim/vimrc
+lndot .zshrc
 
-# -- bin
-ln -sfn ${DOTS}/bin/colorpicker              ${HOME}/bin/colorpicker
-ln -sfn ${DOTS}/bin/scr                      ${HOME}/bin/scr
-ln -sfn ${DOTS}/bin/scrmonitor               ${HOME}/bin/scrmonitor
-ln -sfn ${DOTS}/bin/scrwindow                ${HOME}/bin/scrwindow
-ln -sfn ${DOTS}/bin/pack                     ${HOME}/bin/pack
-ln -sfn ${DOTS}/bin/unpack                   ${HOME}/bin/unpack
+lndot .xkb/symbols/us-german-umlaut
+lndot .local/share/applications
 
+lndot bin/colorpicker
+lndot bin/scr
+lndot bin/scrmonitor
+lndot bin/scrwindow
+lndot bin/pack
+lndot bin/unpack
 
 # TODO: syncthing
